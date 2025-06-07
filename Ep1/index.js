d626e229b17329d3808280bd66d51519146cb293
Chandraveer7389 <chandraveer7389@gmail.com>
2025-06-07 10:11:54 +0530

    upgraded ep1

--- Ep1/index.js
+++ Ep1/index.js
@@ -1,20 +1,11 @@
-/* <div id="parent">
-    <div id=chinld>
-         <h1>hey</h1>
-         <h2>hello</h2>
-    </div>
-    <div id=chinld2>
-         <h1>i am</h1>
-         <h2>veer</h2>
-    </div>
+import React from "react";
+import { createRoot } from "react-dom/client"; 
 
-
-</div> */
 
 const heading = React.createElement("div", { id: "parent" },
     [React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "hey"), React.createElement("h1", {}, "hello")]),
     React.createElement("div", { id: "child2" }, [React.createElement("h1", {}, "i am "), React.createElement("h1", {}, "veer")])
     ])
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = createRoot(document.getElementById("root"))
 root.render(heading)
