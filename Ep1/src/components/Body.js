26f3f7fcf5d634afd16309b3a70cfa8bccb16912
Chandraveer7389 <chandraveer7389@gmail.com>
2025-06-12 13:16:45 +0530

    Added search

--- Ep1/src/components/Body.js
+++ Ep1/src/components/Body.js
@@ -6,9 +6,22 @@ import { rest } from "../utils/constant";
 const Body = () =>{
   let filterOb =[];
   const [List,setaList] = useState(rest);
+  const [searchItems,setSearchItems] = useState("");
     return (
       <div className="body">
         <div className="filter">
+          <div className="search">
+            <input type="text" className="search-box" value={searchItems} onChange={(items) =>{
+              setSearchItems(items.target.value)
+            }}/>
+            <button className="search-btn" onClick={()=>{
+              const additems = rest.filter((obj) =>{
+                return obj.Resname.toUpperCase().includes(searchItems.toUpperCase())
+              })
+              setaList(additems)
+            }}>Search</button>
+
+          </div>
             <button className="filter-btn" onClick={() =>{
                     filterOb=List.filter((obj) => {
                       if(obj.rating > 3.9){
